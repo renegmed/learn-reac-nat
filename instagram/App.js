fa76c273f26ea93dfca1e2e9d26b84c277cbcfc8
rene <renegmed@yahoo.com>
2018-12-31 08:23:02 -0500

    instagram - managing state using redux

--- instagram/App.js
+++ instagram/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { View } from 'react-native';
 import Header from './src/components/Header';
 import PhotoFeed from './src/components/Photo-Feed';
- 
+import { createStore } from 'redux';
+import reducers from './src/reducers'
+
 export default class App extends React.Component {
   
    render() {
     return (
-      <View> 
-        <Header text="Photos" />
-        <PhotoFeed /> 
-      </View>
+      <Provider store={createStore(reducers)} >
+        <View> 
+          <Header text="Photos" />
+          <PhotoFeed /> 
+        </View>
+      </Provider> 
     );
   }
 }
