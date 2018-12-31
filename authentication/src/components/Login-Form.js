abeb628805aab59dd9f54d938512491e113e145c
rene <renegmed@yahoo.com>
2018-12-31 17:15:03 -0500

    authen - firebase configuration

--- authentication/src/components/Login-Form.js
+++ authentication/src/components/Login-Form.js
@@ -1,10 +1,24 @@
 import React from 'react'; 
 import { View } from 'react-native'; 
 import { Button, FormInput } from 'react-native-elements';
-import InnerSection from './Inner-Section';
+import firebase from 'firebase';
+import InnerSection from './Inner-Section'; 
 
 export default class LoginForm extends React.Component {
-  
+    
+    componentDidMount() {
+        const config = {
+            apiKey: "AIzaSyAjH5pEnkugf2EVk9LJSNAPXKle-3Q-jIE",
+            authDomain: "authentication-2d360.firebaseapp.com",
+            databaseURL: "https://authentication-2d360.firebaseio.com",
+            projectId: "authentication-2d360",
+            storageBucket: "authentication-2d360.appspot.com",
+            messagingSenderId: "677531134728"
+          };
+          firebase.initializeApp(config);
+        
+    }
+    
     render() {
         return (
             <View style={styles.container}> 
