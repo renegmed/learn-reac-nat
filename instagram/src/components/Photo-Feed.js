16fbb87f592e38107e83b2a49f3c4f493828827a
rene <renegmed@yahoo.com>
2018-12-31 01:04:08 -0500

    instagram - react states

--- instagram/src/components/Photo-Feed.js
+++ instagram/src/components/Photo-Feed.js
@@ -4,11 +4,16 @@ import axios from 'axios';
 
 export default class PhotoFeed extends Component { 
     render() {
+        this.state = { photos: ['hello'] };
         axios.get('http://localhost:3000/photos')
-            .then(response => console.log(response.data));
+            .then(response => {
+                console.log(response.status);
+                this.setState({ photos: response.data });
+
+            });
         return (
             <View>
-                <Text> textInComponent </Text>
+                <Text> {this.state.photos} </Text>
             </View>
         );
     }
