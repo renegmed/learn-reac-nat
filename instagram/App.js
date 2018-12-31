44bf0a190ec3ec8ce81c92a261aa0a4ed5335b0f
rene <renegmed@yahoo.com>
2018-12-30 22:51:18 -0500

    instagram - flexbox

--- instagram/App.js
+++ instagram/App.js
@@ -2,24 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 import PhotoSection from './src/components/Photo-Section';
+import Dummy from './src/components/Dummy';
 
 export default class App extends React.Component {
   
    render() {
     return (
-      <View>
-        <Header text="Photos"/>
-        <PhotoSection />
+      <View> 
+        <Dummy />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: { 
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',   
-  },
-});
