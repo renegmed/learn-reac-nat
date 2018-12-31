b2d2361f121a97b7aa61baef1c73ce687aa52041
rene <renegmed@yahoo.com>
2018-12-31 01:10:36 -0500

    instagram - lifecycle methods

--- instagram/src/components/Photo-Feed.js
+++ instagram/src/components/Photo-Feed.js
@@ -3,17 +3,21 @@ import { View, Text} from 'react-native';
 import axios from 'axios'; 
 
 export default class PhotoFeed extends Component { 
-    render() {
-        this.state = { photos: ['hello'] };
-        axios.get('http://localhost:3000/photos')
-            .then(response => {
-                console.log(response.status);
-                this.setState({ photos: response.data });
+    state = { photos: [{username: ''}] };
 
-            });
+    componentDidMount() {  // called before render() function is called
+        axios.get('http://localhost:3000/photos')
+        .then(response => {
+            console.log(response.status);
+            this.setState({ photos: response.data });  // triggers re-render
+
+        });
+    }
+
+    render() {  
         return (
             <View>
-                <Text> {this.state.photos} </Text>
+                <Text> {this.state.photos[0].username} </Text>
             </View>
         );
     }
