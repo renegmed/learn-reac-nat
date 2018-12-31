2d48062005c9b832d9ba26c86a8d3eafba9c8ac2
rene <renegmed@yahoo.com>
2018-12-31 08:57:19 -0500

    instagram - action creators

--- instagram/App.js
+++ instagram/App.js
@@ -3,14 +3,18 @@ import { Provider } from 'react-redux';
 import { View } from 'react-native';
 import Header from './src/components/Header';
 import PhotoFeed from './src/components/Photo-Feed';
-import { createStore } from 'redux';
-import reducers from './src/reducers'
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './src/reducers';
+import ReduxThunk from 'redux-thunk';
 
 export default class App extends React.Component {
   
    render() {
+    
+    const store = createStore( reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)} >
+      <Provider store={store} >
         <View> 
           <Header text="Photos" />
           <PhotoFeed /> 
---
+++ instagram/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export function getPhotos() {  // returns a function for async processing by middleware thunk
+    return (dispatch) => {      // whenever getPhotos() is called, this will be called asynchronuously and result is received by reducer
+        axios.get('https://localhost:3000/photos')  
+            .then(response => {
+                dispatch({
+                    type: 'GET_PHOTOS',
+                    payload: response.data
+                });
+            });
+    } 
+}
